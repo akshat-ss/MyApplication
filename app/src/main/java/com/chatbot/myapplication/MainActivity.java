bc7ca5f15f37d2de6886343cf9fc8aea42141b07
akshatshrivastava <akshatshrivastava321@gmail.com>
2023-06-26 03:43:34 +0530

    Finished Project

---
+++ app/src/main/java/com/chatbot/myapplication/MainActivity.java
@@ -0,0 +1,12 @@
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+
+    @Override
+    protected void onCreate
